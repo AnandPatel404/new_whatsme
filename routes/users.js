b31f94fe1f42cd2a0513c01e55abd4c55a1f80c8
NiralPatel0 <niralghetiya0@gmail.com>
2026-02-28 14:55:18 +0530

    whatsapp add page done

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,8 @@ router.get('/dashboard', function (req, res, next) {
     res.render('users/dashboard', { title: 'Dashboard' });
 });
 
+router.get('/add-whatsapp', function (req, res, next) {
+    res.render('users/whatsapp/add-whatsapp', { title: 'Add Whatsapp' });
+});
+
 module.exports = router;
