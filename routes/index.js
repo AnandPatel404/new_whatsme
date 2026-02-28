b9abc1b9aabefbfbce7a48eb49bc08d58135c902
NiralPatel0 <niralghetiya0@gmail.com>
2026-02-28 16:34:45 +0530

    whtsapp add edit and read ui done

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/404', function(req, res, next) {
+  res.render('error/404', { title: 'Error 404' });
+});
+
+router.get('/500', function(req, res, next) {
+  res.render('error/500', { title: 'Error 500' });
+});
+
+router.get('/email', function(req, res, next) {
+  res.render('email/otp', { title: 'Error 500' });
+});
+
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -10,4 +10,14 @@ router.get('/add-whatsapp', function (req, res, next) {
     res.render('users/whatsapp/add-whatsapp', { title: 'Add Whatsapp' });
 });
 
+router.get('/edit-whatsapp', function (req, res, next) {
+    res.render('users/whatsapp/edit-whatsapp', { title: 'Edit Whatsapp' });
+});
+
+
+router.get('/all-whatsapp', function (req, res, next) {
+    res.render('users/whatsapp/all-whatsapp', { title: 'All Whatsapp' });
+});
+
+
 module.exports = router;
